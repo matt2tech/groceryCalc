900d76da791d0f12432f4f36eb79c206fd796205
matt2tech <Matt.Lipsey@elliottlogic.com>
2020-04-02 11:54:05 -0500

    numValues update via keypress

--- core.js
+++ core.js
@@ -1,4 +1,13 @@
-function add(num1, num2) {
+num1 = document.getElementById("num1").value;
+num2 = document.getElementById("num2").value;
+
+document.addEventListener("keypress", function() {
+    num1 = document.getElementById("num1").value;
+    num2 = document.getElementById("num2").value;
+});
+
+function add() {
+    console.log("Starting add")
     tax = parseFloat(document.getElementById("tax").value);
     console.log(`Num1: ${num1}\nNum2: ${num2}\nTax: ${tax}`)
     total = parseInt(num1) + parseInt(num2);
